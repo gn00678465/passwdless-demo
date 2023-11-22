a0e98f137781b8413d5d182b042abe36cbb0024d
Madao <gn00678465@gmail.com>
2023-11-22 09:22:38 +0800

    feat(api): Change challenge router

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
+import challengeRouter from './routes/challenge';
 import registerRouter from './routes/register';
 
 const app = express();
 
+app.use('/', challengeRouter);
 app.use('/register', registerRouter);
 
 export default app;
--- server/src/routes/challenge.ts
+++ server/src/routes/challenge.ts
@@ -1,16 +1,15 @@
 import express, { Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-router.get('/options', (req: Request, res: Response) => {
+router.get('/challenge', (req: Request, res: Response) => {
   res.status(200).json({
     status: 'Success',
     data: {
-      options: {}
+      challenge: uuidv4()
     }
   });
 });
-
-router.post('/verify_registration', (req: Request, res: Response) => {});
 
 export default router;
