be3408930945f8bb81ade69f4f68101ea2e19497
Madao <gn00678465@gmail.com>
2023-11-22 11:05:17 +0800

    feat: Handle CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+
 import challengeRouter from './routes/challenge';
 import registerRouter from './routes/register';
 
 const app = express();
 
-app.use('/', challengeRouter);
-app.use('/register', registerRouter);
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
+  })
+);
+
+app.use('/api', challengeRouter);
+app.use('/api/register', registerRouter);
 
 export default app;
