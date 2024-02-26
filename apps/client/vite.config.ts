6f545b269a835e986b0389a4f2e0784981620a31
Madao <gn00678465@gmail.com>
2024-02-26 15:05:49 +0800

    chore: add unocss support

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import UnoCSS from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), UnoCSS()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     https: {
-      key: '../../certs/server.key',
-      cert: '../../certs/server.crt'
+      key: "../../certs/server.key",
+      cert: "../../certs/server.crt"
     },
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, "")
       }
     }
   }
